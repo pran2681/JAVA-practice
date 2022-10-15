70b2af8aa5be92a5580d3d80b563366ddb04310c
Pran Prachurjya Saikia <92747138+pran2681@users.noreply.github.com>
2022-10-15 16:45:23 +0530

    Update sum.java

--- Function or Methods Java/sum.java
+++ Function or Methods Java/sum.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class sum {
+    //main method
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the two numbee to sum: ");
+        //input
         int a = sc.nextInt();
         int b =sc.nextInt();
 
-        printSum(a, b);
+        printSum(a, b);//method calling
 
         sc.close();
 
